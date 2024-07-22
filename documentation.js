926ed0586e1505b4c9dca693331356208f8bf2fa
Aladi TIMERA <aladi.timera@eren.lan>
2024-07-22 12:19:16 +0200

    first commit

---
+++ documentation.js
@@ -0,0 +1,35 @@
+/* 
+
+((((DÉPLOYER SON APPLICATION REACT NATIVE))))))
+
+Utilitaires utilisés :  github et netifly
+
+
+créer un dépot  sur github du projet app-a-deployer
+
+1 -  Aller sur github et creer le nom du repot et suivre ces etapes : 
+git init                           //initier 
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/digitaladi/app-a-deployer.git
+git push -u origin main
+
+
+et maintenant le code de "app-a-deployer" est dans github
+
+
+2 - Aller dans votre compte netlify si vous avez un compte : https://app.netlify.com/teams/digitaladi/overview
+
+    - cliquer  sur "Import from Git" pour importer le dépot "app-a-deployer" github 
+
+    - cliquer sur "Deploy app-a-deployer"
+    - vous pouvez changer le nom de l'application qu'on va appller fake-nutrinet
+    - définir un nom de domaine qu'on va appeller fake-nutrinet 
+    - securier l'application avec https
+
+       NB:  Pour créer un compte il netlify faut aller sur ton github et fait déployer il te créera un compte netifly avec les identifants de ton ghitbub
+
+
+*/
+
